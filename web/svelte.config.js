a1ae51524a82d0ba8a85cca336d200a377387028
Tyeetale <tyeetale@gmail.com>
2025-11-28 21:56:28 -0800

    add shadcn for svelte

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -9,7 +9,11 @@ const config = {
 	kit: { // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 	// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 	// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-	adapter: adapter() }
+	adapter: adapter(),
+		alias: {
+			'@': path.resolve(__dirname, 'src/lib')
+		}
+	}
 };
 
 export default config;
